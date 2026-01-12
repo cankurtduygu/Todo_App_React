277702bf0e9a928195994e7618d9b503e4522275
Duygu cankurt <dyg.cankurt17@gmail.com>
2026-01-12 22:21:24 +0100

    add todoApp complett

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
-import { FaClipboardList } from 'react-icons/fa';
-import { FaPlus } from 'react-icons/fa';
+import {useEffect, useRef, useState} from 'react';
+import { FaClipboardList, FaPlus } from 'react-icons/fa';
 import Todoitem from './Todoitem';
 
 
 
 const Todo = () => {
+
+  const [todos, setTodos] = useState(localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [] );
+  const data = useRef();
+
+  const addTodos = () =>{
+
+    const inputText = data.current.value.trim();
+
+    if(inputText== ""){
+      return null;
+    }
+    // console.log(data.current.value.trim()); /// Trim input value to avoid empty or space-only todos
+
+    const newTodo = {
+      id: todos.length + 1,
+      text: inputText, 
+      isComplete: false,
+    };
+
+    setTodos((prev) => [...prev, newTodo]);
+
+    data.current.value = "";
+  };
+
+  const toggle = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) =>{
+      return prevTodos.filter(todo => todo.id !== id) })
+    };
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos])
+
+
   return (
     <div className="place-self-center bg-white w-112.5 h-150 p-12 flex flex-col gap-8 rounded-lg">
       {/* Baslik Kismi  */}
@@ -16,24 +57,21 @@ const Todo = () => {
       {/*Arama Kismi*/}
       <div className="flex items-center bg-[#EEEEEE] rounded-full">
         <input
+          ref={data}
           type="text"
           className="border-none outline-none p-3.5 flex-1 bg-transparent placeholder:text-slate-400"
           placeholder="Add a new Task"
         />
-        <div className="bg-[#00ADB5] h-full w-14 flex items-center justify-center rounded-r-full cursor-pointer">
+        <div className="bg-[#00ADB5] h-full w-14 flex items-center justify-center rounded-r-full cursor-pointer" onClick={()=> addTodos()}>
           <FaPlus className="size-6 text-[#EEEEEE]" />
         </div>
       </div>
       
       {/* Listelenen Görevler */}
       <div className='mt-5'>
-        <Todoitem />
-        <Todoitem />
-        <Todoitem />
-        <Todoitem />
-        <Todoitem />
-        
-
+          {todos.map((todo) => (
+            <Todoitem key={todo.id} todo= {todo} toggle={toggle} deleteTodo={deleteTodo} />
+          ))}
       </div>
 
     </div>
--- src/components/Todoitem.jsx
+++ src/components/Todoitem.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { FaRegCircle, FaRegTrashAlt, FaRegCheckCircle } from 'react-icons/fa';
 
-const Todoitem = () => {
+const Todoitem = ({todo, toggle, deleteTodo}) => { 
   return (
-    <div className='w-full flex items-center px-2 py-4 gap-2 border-b pb-4 cursor-pointer'>
-      <FaRegCircle className='text-[#00ADB5] size-5'/>
-      <p className='flex-1'>Lorem ipsum dolor sit</p>
-      <FaRegTrashAlt className='text-[#e01056] size-5 hover:scale-110 transition-all' />
+    <div className='w-full flex items-center px-2 py-4 gap-2 border-b pb-4 cursor-pointer select-none' onClick={()=> toggle(todo.id)}>
+
+    {
+      todo.isComplete ? (
+        <FaRegCheckCircle className='text-[#00ADB5] size-5'/> ) : ( 
+          <FaRegCircle className='text-[#00ADB5] size-5'/>
+    )}
+
+      <p className={`flex-1 ${todo.isComplete ? 'line-through text-gray-500' : ''}`}>{todo.text}</p>
+      <FaRegTrashAlt className='text-[#e01056] size-5 hover:scale-110 transition-all' onClick={() => deleteTodo(todo.id)} />
     </div>
   );
 };
